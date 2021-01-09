9f3aca90138e337eb3b4e5b857558df1431f4e35
Giuseppe Tribulato <giuseppe.tribulato@here.com>
2021-01-09 14:51:28 +0100

    Change license from Apache 2 to MIT

--- setup.py
+++ setup.py
@@ -19,9 +19,10 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ]
 )
