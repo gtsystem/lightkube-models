7404b99f968268079c382adc76ffaeb1127d98d1
Giuseppe Tribulato <gtsystem@gmail.com>
2025-12-25 18:09:24 +0100

    Move core in src. Lint of main

--- lightkube-generate/__main__.py
+++ lightkube-generate/__main__.py
@@ -1,22 +1,41 @@
 from pathlib import Path
 
-from . import compile_resources, compile_models, __version__
-
+from . import __version__, compile_models, compile_resources
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description="Generate resources from k8s API swagger file")
-    parser.add_argument("command", choices=('resources', 'models'), nargs='+', help="Specification file for Kubernetes")
+    parser = argparse.ArgumentParser(
+        description="Generate resources from k8s API swagger file"
+    )
+    parser.add_argument(
+        "command",
+        choices=("resources", "models"),
+        nargs="+",
+        help="Specification file for Kubernetes",
+    )
     parser.add_argument("specs", help="Specification file for Kubernetes")
     parser.add_argument("-d", "--dest", help="Package directory", default="lightkube")
     parser.add_argument("--docs", help="Docs directory", default="docs")
-    parser.add_argument("-t", "--testdir", help="Directory where to generate the test file", default=".")
+    parser.add_argument(
+        "-t", "--testdir", help="Directory where to generate the test file", default="."
+    )
     args = parser.parse_args()
 
     compiler_major = __version__.split(".", 1)[0]
     if "resources" in args.command:
-        compile_resources.execute(Path(args.specs), Path(args.dest), Path(args.testdir), Path(args.docs), compiler_major)
+        compile_resources.execute(
+            Path(args.specs),
+            Path(args.dest),
+            Path(args.testdir),
+            Path(args.docs),
+            compiler_major,
+        )
     if "models" in args.command:
-        compile_models.execute(Path(args.specs), Path(args.dest), Path(args.testdir), Path(args.docs), compiler_major)
-
+        compile_models.execute(
+            Path(args.specs),
+            Path(args.dest),
+            Path(args.testdir),
+            Path(args.docs),
+            compiler_major,
+        )
