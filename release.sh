41a78c0b05ca781677c26800a2ec8ced8e04a37e
Giuseppe Tribulato <gtsystem@gmail.com>
2025-12-25 18:04:08 +0100

    Moving to uv and hatch

--- release.sh
+++ release.sh
@@ -15,10 +15,8 @@ for v in $VERSIONS; do
   python -m lightkube-generate resources models openapi/kubernetes_v$v.json --docs docs
   python test_models.py
   python test_resources.py
-  python setup.py clean --all
-  python setup.py sdist
-  python setup.py bdist_wheel
-  python setup.py sdist
+  rm -rf build
+  uvx hatch build
   python -m mkdocs build -d site/$MAIN_VERSION
   #twine upload dist/lightkube_models-${v}.*-py3-none-any.whl -r $1
   ls dist/lightkube_models-${v}.*-py3-none-any.whl
