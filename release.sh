922bc13e3ae0c91bbcef054e4ea552eb81ddb05b
Giuseppe Tribulato <gtsystem@gmail.com>
2025-12-25 18:45:55 +0100

    Fix release.sh script

--- release.sh
+++ release.sh
@@ -12,13 +12,13 @@ fi
 for v in $VERSIONS; do
   MAIN_VERSION=$(echo $v | cut -d. -f1,2 )
   echo "Building v$v"
-  python -m lightkube-generate resources models openapi/kubernetes_v$v.json --docs docs
-  python test_models.py
-  python test_resources.py
+  uv run --group compile python -m lightkube-generate resources models openapi/kubernetes_v$v.json --docs docs
+  uv run python test_models.py
+  uv run python test_resources.py
   rm -rf build
   uvx hatch build
-  python -m mkdocs build -d site/$MAIN_VERSION
+  uv run --group compile python -m mkdocs build -d site/$MAIN_VERSION
   #twine upload dist/lightkube_models-${v}.*-py3-none-any.whl -r $1
   ls dist/lightkube_models-${v}.*-py3-none-any.whl
-  ls dist/lightkube-models-${v}.*.tar.gz
+  ls dist/lightkube_models-${v}.*.tar.gz
 done
